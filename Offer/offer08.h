f21768cc7547e9253fcf836ad2b70446aa263ba0
XCLSX <444205315@qq.com>
2021-06-20 14:34:15 +0800

    两个栈实现队列

---
+++ Offer/offer08.h
@@ -0,0 +1,33 @@
+class CQueue {
+private:
+    stack<int> sk1;
+    stack<int> sk2;
+public:
+    CQueue() {
+
+    }
+
+    void appendTail(int value) {
+       sk1.push(value);
+    }
+
+    int deleteHead() {
+        if(sk1.size()==0)
+            return -1;
+        int val;
+        while(!sk1.empty())
+        {
+            sk2.push(sk1.top());
+            sk1.pop();
+        }
+        val = sk2.top();
+        sk2.pop();
+        while(!sk2.empty())
+        {
+            sk1.push(sk2.top());
+            sk2.pop();
+        }
+        return val;
+    }
+};
+
